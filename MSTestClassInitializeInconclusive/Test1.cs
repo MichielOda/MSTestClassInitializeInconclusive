1569d81196d89f66ff93211760db1d886e165545
Michiel Oda <michiel.oda@skyline.be>
2025-03-13 10:30:36 +0100

    Showcase of issue

---
+++ MSTestClassInitializeInconclusive/Test1.cs
@@ -0,0 +1,18 @@
+﻿namespace MSTestClassInitializeInconclusive
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [ClassInitialize]
+        public static void ClassInit(TestContext context)
+        {
+            Assert.Inconclusive("Should not fail workflow");
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
